83e2e3323ef850732e3d7edb17851d64f6257e8c
Deepak Dhaker <ddhaker2000@gmail.com>
2024-08-11 19:55:23 +0530

    cors error fixed

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -9,33 +9,33 @@ const Authenticate = async (req, res, next ) => {
     try{
 
         const token = req.cookies.jwttoken;
-        // console.log("token in authentcate middleware -- token : "+token);
+        console.log("token in authentcate middleware -- token : "+token);
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-        // console.log("verify token : " + verifyToken);
+        console.log("verify token : " + verifyToken);
 
         const rootUser = await User.findOne({ 
             _id: verifyToken._id, 
             "tokens.token": token 
         });
-        // console.log("rootuse is : ");
-        // console.log(rootUser);
+        console.log("rootuse is : ");
+        console.log(rootUser);
 
         if(!rootUser) {
-            // console.log("user not found in authenticate");
+            console.log("user not found in authenticate");
             throw new Error('User not found');
         }
 
         req.token = token;
         req.rootUser = rootUser;
         req.userID = rootUser._id;
-        // console.log("no error found in authenticate.jss");
+        console.log("no error found in authenticate.jss");
 
         next();
 
     } catch (err) {
         console.log(err);
         res.status(401).send('Unauthorized : No token provided error');
-        // res.status(401).send(err + "Unauthoriized");
+        res.status(401).send(err + "Unauthoriized");
         
     }
 }
