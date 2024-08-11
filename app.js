a0ccb58723afb86899f22cde9ef64d996f9e847b
Deepak Dhaker <ddhaker2000@gmail.com>
2024-08-11 23:37:20 +0530

    bug fix

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const cookieParser = require('cookie-parser');
 // };
 
 // app.use(cors(corsOptions));
-const cors = require('cors');
 app.use(cors({
   origin: "https://justdancef.onrender.com", // The frontend URL
   credentials: true
